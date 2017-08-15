0210c68a065bfc4720ca916002590ed4eb5b5647
lpshahim <lp.shahim6@gmail.com>
2017-08-15 11:10:13 +0200

    Read 8 pixels at a time, and implement confidence match for biometric transforms

---
+++ Read8Pixels/Read8Pixels/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Read8Pixels
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			int[] num = { 1, 2, 3, 4, 5 };
+			int[] user = new int[5];
+			string input = "";
+			int match = 0;
+			double percentage = 0.0;
+
+			for (int a = 0; a<5;a++){
+				input = Console.ReadLine();
+				user[a] = Convert.ToInt32(input);
+			}
+
+			for (int i = 0; i < num.Length; i++) {
+				if (num[i] == user[i])
+				{
+					Console.WriteLine("Match");
+					match++;
+					percentage = (Convert.ToDouble(match) / Convert.ToDouble(user.Length));
+				}
+				else {
+					Console.WriteLine("No match");
+				}
+			}
+
+			Console.WriteLine("Percentage match: " + percentage);
+
+			//TODO: READ 8 PIXELS AT A TIME
+
+
+		}
+	}
+}
